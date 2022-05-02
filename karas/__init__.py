29b94f3d4be2c7bf168f0bd9c3fc808d4bb921ee
ShiroDoMain <1808107177@qq.com>
2022-05-02 14:23:05 +0800

    追加功能

--- karas/__init__.py
+++ karas/__init__.py
@@ -1,14 +1,28 @@
-from aiohttp import ClientSession, WSMsgType
+from aiohttp import ClientSession
 import asyncio
 from typing import List, Optional, Union, AsyncGenerator
 from .chain import MessageChain
-from .Sender import Friend, Group, Member, Stranger
+from .Sender import Friend, Group, Member, Stranger, ReceptorBase
 from .messages import MessageBase
 from .event import Auto_Switch_Event, EventBase, RequestEvent, Event, NudgeEvent
-from .elements import ElementBase, File, FlashImage, Image, Plain, Source, Voice
+from .elements import ElementBase, File, FlashImage, Image, Plain, Source, Voice, FriendProfile, MemberProfile, \
+    BotProfile, UserProfile
 from .exceptions import InvalidArgumentError, VerifyError
 from .util.Logger import Logging
-from .util.network import error_throw, URL_Route, send_json
+from .util.network import error_throw, URL_Route, wrap_data_json, echo_receiver
+
+
+async def _build_content_json(
+        _type: str,
+        _obj: Union[ReceptorBase, int],
+        _quote: Optional[Source],
+        _chain: List
+):
+    return {
+        _type: _obj if isinstance(_obj, int) else _obj.id,
+        "quote": _quote and _quote.id,
+        "messageChain": _chain
+    }
 
 
 class Karas(object):
@@ -19,7 +33,7 @@ class Karas(object):
     listeners = {}
 
     @classmethod
-    async def event_parse(cls, original: dict,_logger:Logging=None) -> AsyncGenerator:
+    async def event_parse(cls, original: dict, _logger: Logging = None) -> AsyncGenerator:
         # print(f"\nOriginal:{original}\n")
         _event: Union[MessageBase, Event] = Auto_Switch_Event.parse_json(**original)
         isBotEvent = yield _event
@@ -94,7 +108,7 @@ class Yurine(object):
             self.session = ClientSession(loop=self.loop)
             await self._connect()
             self.logging.info("Account verify success")
-            self.logging.debug(f"get verifyKey {self.sessionKey}")
+            self.logging.debug(f"got verifyKey {self.sessionKey}")
 
             # async with self.session.post(
             #         "/bind",
@@ -114,7 +128,7 @@ class Yurine(object):
     async def _release(self):
         """
         对当前bot持有的session key进行释放请求
-            NOTE:由于使用了ws接口,无需对sesskey进行释放
+            NOTE:由于使用了ws接口,无需对sessionKey进行释放
         """
         async with self.session.post(
                 "/release",
@@ -152,14 +166,19 @@ class Yurine(object):
         :raise VerifyError
         """
         self.logging.debug(f"connect websocket server {self.url}")
-        self.ws = await self.session.ws_connect(url=self.route("all"),
-                                                headers={"verifyKey": self.verifyKey, "qq": str(self.account)})
+        self.ws = await self.session.ws_connect(
+            url=self.route("all"),
+            headers={
+                "verifyKey": self.verifyKey,
+                "qq": str(self.account)
+            }
+        )
         _verify_response: dict = await self.ws.receive_json()
         self.logging.debug(f"verify key {_verify_response}")
         _verify_data: dict = _verify_response.get("data")
         if _verify_data.get("code") != 0:
             raise VerifyError
-        self.sessionKey: str = _verify_data["session"]
+        self.sessionKey: str = _verify_data.get("session")
         self.loop.create_task(self._ping())
         return 0
 
@@ -169,12 +188,12 @@ class Yurine(object):
         while True:
             _receive_data: dict = await self.ws.receive_json()
             if _receive_data.get("syncId") == "-1":
-                _parser = self.karas.event_parse(_receive_data["data"],self.logging)
+                _parser = self.karas.event_parse(_receive_data["data"], self.logging)
                 _event = await _parser.__anext__()
                 await _parser.asend(self.account == _event.event.fromId) \
                     if isinstance(_event, Event) else await _parser.asend(False)
 
-    def listen(self, registerEvent: Union[str, "EventBase", "MessageBase"]) -> None:
+    def listen(self, registerEvent: Union[str, "EventBase", "MessageBase"]):
         """事件装饰器
         Args:
             registerEvent (str, Event, Message): 要监听的事件或者消息类型
@@ -201,7 +220,7 @@ class Yurine(object):
     async def accept(self, requestEvent: "RequestEvent", message: str = None) -> None:
         requestEvent.message = message
         await self.ws.send_json(
-            send_json(
+            wrap_data_json(
                 syncId="accept",
                 command=requestEvent.command,
                 content=requestEvent.accept
@@ -222,50 +241,136 @@ class Yurine(object):
             obj(**_response_json)
 
     async def _element_check(self, element: "ElementBase", type: str):
-        if isinstance(element, (Image, Voice)):
+        if isinstance(element, (Image, Voice, File, FlashImage)):
             await self.uploadMultipart(element, type=type)
-        return element.element
+        return element.elements
 
     @error_throw
-    async def send_group(self, group: Union[int, "Group"],
-                         Elements: Union[List[Union[ElementBase, MessageChain]], MessageChain],
-                         quote: Union[int, Source] = None) -> int:
+    async def sendGroup(
+            self, group: Union[int, "Group"],
+            Elements: Union[List[Union[ElementBase, MessageChain]], MessageChain],
+            quote: Union[int, Source] = None
+    ) -> Optional[int]:
         """发送群组消息
 
 
         Args:
-            Elements (list,Element): 要发送的消息类型，可以是单个类型或者一个列表
             group (Union[int,Group]): 要发送的群组id或者对象
+            Elements (list,Element): 要发送的消息类型，可以是单个类型或者一个列表
             quote (Union[int,Source]): 引用一条消息的messageId进行回复
 
         Returns:
             int: 一个Int类型属性，标识本条消息，用于撤回和引用回复
         """
-        _chain = [
-            (await self._element_check(_e, type="group")) for _e in Elements] \
+        _chain = [(await self._element_check(_e, type="group")) for _e in Elements] \
             if not isinstance(Elements, MessageChain) else Elements.parse_to_json()
-        wrapper_json = {
-            "sessionKey": self.sessionKey,
+        content = {
             "group": group if isinstance(group, int) else group.id,
-            "quote": quote,
+            "quote": quote.id,
             "messageChain": _chain
         }
-        self.logging.info(f"=> {[str(_e) for _e in _chain]}")
         await self.ws.send_json(
-            send_json(
+            wrap_data_json(
                 syncId="sendGroupMessage",
                 command="sendGroupMessage",
-                content=wrapper_json
+                content=content
             ))
-        _response = await self.ws.receive_json()
-        return _response.get("messageId")
+        self.logging.info(f"Bot => {[str(_e) for _e in _chain]}")
+        echo = await self.ws.receive_json()
+        return echo.get("messageId")
 
     @error_throw
-    async def send_nudge(self,
-                         target: Union[int, "ElementBase", "Friend", "Member", None] = None,
-                         subject: Union[int, "Group", "Friend", None] = None,
-                         kind: Union[str, "Group", "Friend", "Stranger", None] = None,
-                         nudgeEvent: "NudgeEvent" = None, ) -> None:
+    async def sendFriend(
+            self,
+            friend: Union[Friend, int],
+            Elements: Union[List[Union[ElementBase, MessageChain]], MessageChain],
+            quote: Optional[Source] = None
+    ) -> Optional[int]:
+
+        """发送好友消息
+
+
+        Args:
+            friend (Union[int,friend]): 要发送的好友id或者对象
+            Elements (list,Element): 要发送的消息类型，可以是单个类型或者一个列表
+            quote (Union[int,Source]): 引用一条消息的messageId进行回复
+
+        Returns:
+            int: 一个Int类型属性，标识本条消息，用于撤回和引用回复
+        """
+        _chain = [(await self._element_check(_e, type="friend")) for _e in Elements] \
+            if not isinstance(Elements, MessageChain) else Elements.parse_to_json()
+        content = await _build_content_json("target", friend, quote, _chain)
+        await self.ws.send_json(
+            wrap_data_json(
+                syncId="sendFriendMessage",
+                command="sendFriendMessage",
+                content=content
+            )
+        )
+        self.logging.info(f"Bot => {[str(_e) for _e in _chain]}")
+        echo = await self.ws.receive_json()
+        return echo.get("messageId")
+
+    @error_throw
+    async def sendTemp(
+            self,
+            member: Union[int, Member],
+            group: Union[int, Group],
+            Elements: Union[List[Union[ElementBase, MessageChain]], MessageChain],
+            quote: Optional[Source] = None
+    ) -> Optional[int]:
+
+        """发送临时会话消息
+
+
+        Args:
+            group (Union[int,Group]): 对象所在的群组
+            member (Union[int,Member]): 要发送的对象id或者成员对象
+            Elements (list,Element): 要发送的消息类型，可以是单个类型或者一个列表
+            quote (Union[int,Source]): 引用一条消息的messageId进行回复
+
+        Returns:
+            int: 一个Int类型属性，标识本条消息，用于撤回和引用回复
+        """
+        _chain = [(await self._element_check(_e, type="temp")) for _e in Elements] \
+            if not isinstance(Elements, MessageChain) else Elements.parse_to_json()
+        content = {
+            "qq": member if isinstance(member, int) else member.id,
+            "group": group if isinstance(member, int) else group.id,
+            quote: quote and quote.id,
+            "messageChain": _chain
+        }
+        await self.ws.send_json(
+            wrap_data_json(
+                syncId="sendTempMessage",
+                command="sendTempMessage",
+                content=content
+            )
+        )
+        echo = await self.ws.receive_json()
+        return echo.get("messageId")
+
+    @error_throw
+    async def recall(self, message: Union[Source, int]):
+        await self.ws.send_json(
+            wrap_data_json(
+                command="recall",
+                syncId="recall",
+                content={
+                    "messageId": message.id if isinstance(message, Source) else message
+                }
+            )
+        )
+
+    @error_throw
+    async def sendNudge(
+            self,
+            target: Union[int, "ElementBase", "Friend", "Member", None] = None,
+            subject: Union[int, "Group", "Friend", None] = None,
+            kind: Union[str, "Group", "Friend", "Stranger", None] = None,
+            nudgeEvent: "NudgeEvent" = None,
+    ) -> None:
         """发送头像戳一戳消息
 
         Args:
@@ -283,7 +388,7 @@ class Yurine(object):
         kind = kind if nudgeEvent is None else nudgeEvent.subject.kind \
             if kind is None else target.group.type if isinstance(target, Member) else target.type
         await self.ws.send_json(
-            send_json(
+            wrap_data_json(
                 syncId="sendNudge",
                 command="sendNudge",
                 content={
@@ -291,10 +396,114 @@ class Yurine(object):
                     "subject": subject if isinstance(subject, int) else subject.id,
                     "kind": kind if isinstance(kind, str) else kind.type
                 }))
-        _response: dict = await self.ws.receive_json()
-        if _response.get("data").get("code") != 0:
-            self.logging.error(f"{_response}")
-        return _response.get("data").get("code")
+        echo: dict = await self.ws.receive_json()
+        if echo.get("data").get("code") != 0:
+            self.logging.error(f"{echo}")
+        return echo.get("data").get("code")
+
+    async def fetchFriendList(self) -> Optional[List[Friend]]:
+        """
+        获取好友列表
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="friendList"
+            )
+        )
+        echo = await self.ws.receive_json()
+        data = echo.get("data")
+        return data and [Friend(**friend) for friend in data.get("data")]
+
+    async def fetchFriendProfile(self, friend: Union[Friend, int]) -> Optional[Friend]:
+        """
+        获取好友详细资料
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="friendProfile",
+                content={
+                    "target": friend if isinstance(friend, int) else friend.id
+                }
+            )
+        )
+        echo = await self.ws.receive_json()
+        friend = echo.get("data")
+        return friend and FriendProfile(**friend)
+
+    async def fetchGroupList(self) -> Optional[List[Group]]:
+        """
+        获取群列表
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="groupList"
+            )
+        )
+        echo = await self.ws.receive_json()
+        data = echo.get("data")
+        return data and [Group(**group) for group in data.get("data")]
+
+    async def fetchMemberList(self, group: [Group, int]) -> Optional[List[Member]]:
+        """
+        获取群成员列表
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="memberList",
+                content={
+                    "target": group if isinstance(group, int) else group.id
+                }
+            )
+        )
+        echo = await self.ws.receive_json()
+        data = echo.get("data")
+        return data and [Member(**member) for member in data.get("data")]
+
+    async def fetchMemberProfile(self, group: Union[Group, int], member: [Member, int]) -> Optional[MemberProfile]:
+        """
+        获取成员详细资料
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="memberProfile",
+                content={
+                    "target": group if isinstance(group, int) else group.id,
+                    "memberId": member if isinstance(member, int) else member.id
+                }
+            )
+        )
+        echo = await self.ws.receive_json()
+        data = echo.get("data")
+        return data and MemberProfile(**data)
+
+    async def fetchBotProfile(self) -> Optional[BotProfile]:
+        """
+        获取bot详细资料
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="botProfile"
+            )
+        )
+        echo = await self.ws.receive_json()
+        data = echo.get("data")
+        return data and BotProfile(**data)
+
+    async def fetchUserProfile(self, target: int) -> Optional[UserProfile]:
+        """
+        获取用户详细资料
+        """
+        await self.ws.send_json(
+            wrap_data_json(
+                command="userProfile",
+                content={
+                    "target": target
+                }
+            )
+        )
+        echo = await self.ws.receive_json()
+        data = echo.get("data")
+        return data and UserProfile(**data)
 
     async def _raise_task_cancel(self, _task: asyncio.Task) -> None:
         """取消当前事件循环中的任务"""
@@ -314,6 +523,7 @@ class Yurine(object):
             self.loop.run_forever()
         except KeyboardInterrupt:
             self.logging.info("Bot close...")
+            raise
         finally:
             self.close()
 
@@ -349,7 +559,8 @@ class Yurine(object):
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         self.logging.debug("exit service")
-        self.close()
+        await self.stop()
+        self.logging.info("exiting...")
 
     def __enter__(self):
         self.loop.run_until_complete(self._initialization())
