ba38e40a43c81f2f4863bcf8ba619f51bb5a04b6
ShiroDoMain <1808107177@qq.com>
2023-01-07 19:07:17 +0800

    fix import

--- karas/permission.py
+++ karas/permission.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from karas.util import BaseModel
 
 
 class Permission:
@@ -8,7 +7,7 @@ class Permission:
     def __init__(self, *args, **kwargs) -> None:
         pass
 
-    def __eq__(self, __o: BaseModel) -> bool:
+    def __eq__(self, __o) -> bool:
         if isinstance(__o, self.__class__):
             return __o.type.upper() == self.type.upper()
         return self.type == __o if isinstance(__o, str) else self.type == __o.type
